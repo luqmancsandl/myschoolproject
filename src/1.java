5d4d21601fefd92a35cc83748b2b9676eb25b9d5
luqmancsandl <luqmancsandl@gmail.com>
2025-03-05 19:07:09 +0000

    Add 1.java

---
+++ src/1.java
@@ -0,0 +1,6 @@
+public class RandomNumber {
+    public static void main(String[] args) {
+        int randNum = (int)(Math.random() * 10 + 1);
+        System.out.println("Random number: " + randNum);
+    }
+}
