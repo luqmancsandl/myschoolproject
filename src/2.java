7197240debe997fb6f445d99621fb700fda1fd48
luqmancsandl <luqmancsandl@gmail.com>
2025-03-06 18:10:20 +0000

    Add 2.java

---
+++ src/2.java
@@ -0,0 +1,9 @@
+package com.myschoolproject;
+
+import java.util.Random;
+
+public class RandomNumber {
+    public static void main(String[] args) {
+        System.out.println("This is a random number: " + new Random().nextInt(10));
+    }
+}
