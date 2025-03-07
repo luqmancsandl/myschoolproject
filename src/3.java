2bee2824fde156d49d5255463af11e45dff6cc70
luqmancsandl <luqmancsandl@gmail.com>
2025-03-07 22:58:32 +0000

    Add 3.java

---
+++ src/3.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class MySchoolProject {
+    public static void main(String[] args) {
+        // Generate a random number between 1 and 10
+        int randNum = (int)(Math.random()*10)+1;
+        
+        // Print the random number to the console
+        System.out.println("Random Number: " + randNum);
+    }
+}
