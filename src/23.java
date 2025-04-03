d096b5e8e45d566bb712360cd7b9c2be2e74b340
luqmancsandl <luqmancsandl@gmail.com>
2025-04-03 13:31:18 +0000

    Add 23.java

---
+++ src/23.java
@@ -0,0 +1,6 @@
+public class MySchoolProject {
+    public static void main(String[] args) {
+        // Example code snippet to demonstrate functionality
+        System.out.println("Hello, World!");
+    }
+}
