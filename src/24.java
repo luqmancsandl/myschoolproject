e018c69d5b8ef41972f9853111d7f341b226b561
luqmancsandl <luqmancsandl@gmail.com>
2025-04-05 04:52:21 +0000

    Add 24.java

---
+++ src/24.java
@@ -0,0 +1,11 @@
+public class SchoolProject {
+    public static void main(String[] args) {
+        // Example of printing "Hello World" in Java
+        System.out.println("Hello World");
+        
+        // Example of using a variable
+        String name = "Alice";
+        int age = 20;
+        System.out.println("Name: " + name + ", Age: " + age);
+    }
+}
